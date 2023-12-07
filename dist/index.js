e83026c6240a13020d401d8155c62b86f2e6b72b
ana.popova <ana.popova@yara.com>
2023-12-07 17:29:59 +0200

    feat(design): rearrange html content

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,6 @@
 // https://www.omdbapi.com/
 // OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=e9f54ad  API KEY
 const movieName = '';
-// function getMovieByName(): void {
-//   const movieName: HTMLElement = document.getElementById("movieTitle") as HTMLElement;
-//   const url: string = `http://www.omdbapi.com/?s=${movieName}&apikey=e9f54ad`;
-//   fetchMovies(url);
-// }
-// function getMoviesByYear(): void {
-//   const url: string = "http://www.omdbapi.com/?s=Barbie&apikey=e9f54ad&type=movie";
-//   fetchMovies(url, true);
-// }
 function getMovieByName() {
     const movieNameInput = document.getElementById("movieTitleInput");
     const movieName = movieNameInput.value.trim();
@@ -40,7 +31,6 @@ function fetchMovies(url, sortByYear = false) {
         .catch((error) => console.error('Error fetching data:', error));
 }
 function displayMovies(movies) {
-    console.log(movies);
     const movieContainer = document.querySelector('.movie-list');
     // Clear previous content
     if (movieContainer !== null) {
@@ -59,3 +49,99 @@ function displayMovies(movies) {
         }
     }
 }
+// MOVIE POSTER
+function displayMoviePoster(posterUrl) {
+    const image = document.getElementById("image");
+    // Update the image source with the movie poster URL
+    if (image instanceof HTMLImageElement) {
+        if (posterUrl !== "N/A") {
+            image.src = posterUrl;
+        }
+        else {
+            alert("No poster available for the selected movies");
+        }
+    }
+}
+function fetchMoviePoster() {
+    const movieNameInput = document.getElementById("movieTitleInput");
+    const movieName = movieNameInput.value.trim();
+    if (!movieName) {
+        alert("Please enter a movie name");
+        return;
+    }
+    const url = `http://www.omdbapi.com/?apikey=e9f54ad&s=${movieName}&type=movie`;
+    fetch(url)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (data) {
+        if (data.Search && data.Search.length > 0) {
+            // Get the first movie from the search results
+            const firstMovie = data.Search[0];
+            let image = document.getElementById("image");
+            // Update the image source with the movie poster URL
+            if (image instanceof HTMLImageElement) {
+                const posterUrl = firstMovie.Poster;
+                if (posterUrl !== "N/A") {
+                    image.src = posterUrl;
+                }
+                else {
+                    alert("No poster available for the selected movie");
+                }
+            }
+        }
+        else {
+            alert(`No search results found for the movie: ${movieName}`);
+        }
+    })
+        .catch(function (error) {
+        alert(`Error fetching movie picture for ${movieName}`);
+    });
+}
+let currentPosterIndex = 0; // Track the current index of the displayed poster
+function fetchNextMoviePoster() {
+    const movieNameInput = document.getElementById("movieTitleInput");
+    const movieName = movieNameInput.value.trim();
+    if (!movieName) {
+        console.error("Please enter a movie name");
+        return;
+    }
+    const apiUrl = `http://www.omdbapi.com/?apikey=e9f54ad&s=${movieName}&type=movie`;
+    fetch(apiUrl)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (data) {
+        if (data.Search && data.Search.length > 0) {
+            // Update the current index to the next poster
+            currentPosterIndex = (currentPosterIndex + 1) % data.Search.length;
+            // Get the movie at the current index
+            const currentMovie = data.Search[currentPosterIndex];
+            displayMoviePoster(currentMovie.Poster);
+        }
+        else {
+            console.error(`No search results found for the movie: ${movieName}`);
+        }
+    })
+        .catch(function (error) {
+        console.error(`Error fetching movie picture for ${movieName}:`, error);
+    });
+}
+//SEARCH SECTION
+const searchSection = document.getElementById('search-section');
+//RESULT SECTION ELEMENTS
+const fetchPoster = document.getElementById('result-section');
+const posterButtons = document.createElement('div');
+posterButtons.classList.add('flex-container');
+const fetchMoviePosterButton = document.createElement('a');
+fetchMoviePosterButton.classList.add('button');
+fetchMoviePosterButton.textContent = 'See Poster';
+posterButtons.appendChild(fetchMoviePosterButton);
+const switchMoviePosterButton = document.createElement('a');
+switchMoviePosterButton.classList.add('button');
+switchMoviePosterButton.textContent = 'Next';
+posterButtons.appendChild(switchMoviePosterButton);
+fetchPoster.appendChild(posterButtons);
+//EVENT LISTENERS
+fetchMoviePosterButton.addEventListener('click', fetchMoviePoster);
+switchMoviePosterButton.addEventListener('click', fetchNextMoviePoster);
--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,6 @@ type Movie = {
 
 const movieName: string = '';
 
-
-// function getMovieByName(): void {
-
-//   const movieName: HTMLElement = document.getElementById("movieTitle") as HTMLElement;
-
-//   const url: string = `http://www.omdbapi.com/?s=${movieName}&apikey=e9f54ad`;
-//   fetchMovies(url);
-// }
-
-// function getMoviesByYear(): void {
-//   const url: string = "http://www.omdbapi.com/?s=Barbie&apikey=e9f54ad&type=movie";
-//   fetchMovies(url, true);
-// }
-
 function getMovieByName(): void {
   const movieNameInput: HTMLInputElement = document.getElementById("movieTitleInput") as HTMLInputElement;
   const movieName: string = movieNameInput.value.trim();
@@ -59,7 +45,6 @@ function fetchMovies(url: string, sortByYear: boolean = false): void {
 
 
 function displayMovies(movies: Movie[]): void {
-  console.log(movies);
   const movieContainer: HTMLElement = document.querySelector('.movie-list') as HTMLElement;
 
   // Clear previous content
@@ -81,3 +66,119 @@ function displayMovies(movies: Movie[]): void {
     }
   }
 }
+
+
+// MOVIE POSTER
+
+function displayMoviePoster(posterUrl: string): void {
+  const image = document.getElementById("image") as HTMLImageElement;
+
+  // Update the image source with the movie poster URL
+  if (image instanceof HTMLImageElement) {
+    if (posterUrl !== "N/A") {
+      image.src = posterUrl;
+    } else {
+      alert("No poster available for the selected movies");
+    }
+  }
+}
+
+function fetchMoviePoster(): void {
+  const movieNameInput: HTMLInputElement = document.getElementById("movieTitleInput") as HTMLInputElement;
+  const movieName: string = movieNameInput.value.trim();
+
+  if (!movieName) {
+    alert("Please enter a movie name");
+    return;
+  }
+
+
+  const url: string = `http://www.omdbapi.com/?apikey=e9f54ad&s=${movieName}&type=movie`;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.Search && data.Search.length > 0) {
+        // Get the first movie from the search results
+        const firstMovie = data.Search[0];
+
+        let image = document.getElementById("image") as HTMLImageElement;
+
+        // Update the image source with the movie poster URL
+        if (image instanceof HTMLImageElement) {
+          const posterUrl = firstMovie.Poster;
+          if (posterUrl !== "N/A") {
+            image.src = posterUrl;
+          } else {
+            alert("No poster available for the selected movie");
+          }
+        }
+      } else {
+        alert(`No search results found for the movie: ${movieName}`);
+      }
+    })
+    .catch(function (error): void {
+      alert(`Error fetching movie picture for ${movieName}`);
+    });
+}
+
+let currentPosterIndex = 0; // Track the current index of the displayed poster
+function fetchNextMoviePoster(): void {
+  const movieNameInput: HTMLInputElement = document.getElementById("movieTitleInput") as HTMLInputElement;
+  const movieName: string = movieNameInput.value.trim();
+
+  if (!movieName) {
+    console.error("Please enter a movie name");
+    return;
+  }
+
+  const apiUrl: string = `http://www.omdbapi.com/?apikey=e9f54ad&s=${movieName}&type=movie`;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.Search && data.Search.length > 0) {
+        // Update the current index to the next poster
+        currentPosterIndex = (currentPosterIndex + 1) % data.Search.length;
+
+        // Get the movie at the current index
+        const currentMovie = data.Search[currentPosterIndex];
+
+        displayMoviePoster(currentMovie.Poster);
+      } else {
+        console.error(`No search results found for the movie: ${movieName}`);
+      }
+    })
+    .catch(function (error): void {
+      console.error(`Error fetching movie picture for ${movieName}:`, error);
+    });
+}
+
+//SEARCH SECTION
+const searchSection: HTMLElement = document.getElementById('search-section') as HTMLElement;
+
+//RESULT SECTION ELEMENTS
+const fetchPoster: HTMLElement = document.getElementById('result-section') as HTMLElement;
+const posterButtons: HTMLDivElement = document.createElement('div');
+posterButtons.classList.add('flex-container');
+
+const fetchMoviePosterButton: HTMLAnchorElement = document.createElement('a');
+fetchMoviePosterButton.classList.add('button');
+fetchMoviePosterButton.textContent = 'See Poster';
+posterButtons.appendChild(fetchMoviePosterButton);
+
+const switchMoviePosterButton: HTMLAnchorElement = document.createElement('a');
+switchMoviePosterButton.classList.add('button');
+switchMoviePosterButton.textContent = 'Next';
+posterButtons.appendChild(switchMoviePosterButton);
+
+fetchPoster.appendChild(posterButtons);
+
+
+//EVENT LISTENERS
+fetchMoviePosterButton.addEventListener('click', fetchMoviePoster); 
+switchMoviePosterButton.addEventListener('click', fetchNextMoviePoster); 
